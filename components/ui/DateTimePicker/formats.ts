ea78144fdda830e2a778c61917e36cd7ba4250d3
Boris Nanista <boris.nanista@01people.com>
2021-12-09 09:42:35 +0100

    fix: fix dates format in picker

--- components/ui/DateTimePicker/formats.ts
+++ components/ui/DateTimePicker/formats.ts
@@ -5,9 +5,9 @@ type TIconsName = keyof typeof MaterialIcons['glyphMap']
  * Display format for date picker moment display placeholders
  */
 export const displayFormats: Record<'date' | 'time' | 'datetime', string> = {
-  date: 'd.M.yyyy',
+  date: 'dd.MM.yyyy',
   time: 'HH:mm',
-  datetime: 'd.m.yyyy HH:mm',
+  datetime: 'dd.MM.yyyy HH:mm',
 }
 
 export const IconMap = new Map<'date' | 'time' | 'datetime', TIconsName>([
