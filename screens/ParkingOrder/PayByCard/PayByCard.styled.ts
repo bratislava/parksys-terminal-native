69672517d1c3f65846f0b390d6b6efa4b9e9cfe9
Martin Pinter <martin.pinter@vacuumlabs.com>
2021-12-23 11:11:19 +0100

    Fix missing PayByCard styled components

--- screens/ParkingOrder/PayByCard/PayByCard.styled.ts
+++ screens/ParkingOrder/PayByCard/PayByCard.styled.ts
@@ -3,3 +3,19 @@ import styled from 'styled-components/native'
 export const PayByCardSC = styled.View`
   flex: 1;
 `
+
+export const ButtonWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  border-top-width: 1px;
+  border-top-color: ${({ theme }) => theme.colors.gray};
+  padding: 16px;
+`
+
+export const HomeSC = styled.Image`
+  width: 30px;
+  height: 30px;
+`
