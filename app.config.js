90f125993893e14814081ab9d1f9b62c6a7af02b
Adam Grund <adam.grund@gmail.com>
2022-05-03 14:19:31 +0200

    RU1 zone udrid 2001 to 2011, bump version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   name: 'Parksys terminal',
   owner: 'bratislava',
   slug: 'parksys-terminal',
-  version: '1.0.14',
+  version: '1.0.16',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',
@@ -24,7 +24,7 @@ export default {
   android: {
     softwareKeyboardLayoutMode: 'pan',
     package: 'com.bratislava.parksysterminal',
-    versionCode: 14,
+    versionCode: 16,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
