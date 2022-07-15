5cb7468a2a6977cde959796f41a8fe79bebd9ab6
Adam Grund <adam.grund@gmail.com>
2022-07-15 11:13:37 +0200

    PTF-30 bump up version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   name: 'Parksys terminal',
   owner: 'bratislava',
   slug: 'parksys-terminal',
-  version: '1.0.22',
+  version: '1.0.23',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',
@@ -25,7 +25,7 @@ export default {
   android: {
     softwareKeyboardLayoutMode: 'pan',
     package: 'com.bratislava.parksysterminal',
-    versionCode: 22,
+    versionCode: 23,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
