503758c1a14c86d685309047f18be5ed810b7c35
Adam Grund <adam.grund@gmail.com>
2022-10-03 13:44:47 +0200

    erase `--no-publish` in building production apk command

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   name: 'Parksys terminal',
   owner: 'bratislava',
   slug: 'parksys-terminal',
-  version: '1.0.25',
+  version: '1.0.26',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',
@@ -25,7 +25,7 @@ export default {
   android: {
     softwareKeyboardLayoutMode: 'pan',
     package: 'com.bratislava.parksysterminal',
-    versionCode: 25,
+    versionCode: 26,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
