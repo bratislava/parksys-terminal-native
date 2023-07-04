6d675aaf155284afad82a0538f338192e58f9ddc
Adam Grund <adam.grund@gmail.com>
2023-07-04 23:04:59 +0200

    reverse previous package upgrades

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'Parksys terminal',
     slug: 'parksys-terminal',
-    version: '1.0.34',
+    version: '1.0.35',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'myapp',
@@ -42,7 +42,7 @@ export default {
     android: {
       softwareKeyboardLayoutMode: 'pan',
       package: 'com.bratislava.parksysterminal',
-      versionCode: 34,
+      versionCode: 35,
       adaptiveIcon: {
         foregroundImage: './assets/images/adaptive-icon.png',
         backgroundColor: '#ffffff',
