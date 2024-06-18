6036ab69b6548961ec53852f3c6a451150be760e
Adam Grund <adam.grund@gmail.com>
2024-06-18 10:00:09 +0200

    erase zone with udrid 2034

--- constants/udrs.ts
+++ constants/udrs.ts
@@ -1601,23 +1601,6 @@ const UDRS: IUdrInfo[] = [
     shapeLength: '148,7956573322',
     vyhradene: 'verejné',
   },
-  {
-    id: 800,
-    mestskaCast: 'Ružinov',
-    kodZony: 'RU3',
-    nazov: 'Parkovisko zimný štadión Ružinov',
-    maxParkovanie: '0',
-    bezpPark: '0',
-    obmedzenePreApk: 'N/A',
-    vynimkaZoSpoplatnenia: 'workdays 12-24',
-    vynimkaApk: 'N/A',
-    udrid: '2034',
-    // THESE ARE NOT VALID, BUT ARE NOT USED EITHER
-    globalIdString: '{645aeb5a-a89d-482c-b13c-6d956cd9b9c3}',
-    shapeArea: '684,914045206256',
-    shapeLength: '148,7956573322',
-    vyhradene: 'verejné',
-  },
 ]
 
 export default UDRS
